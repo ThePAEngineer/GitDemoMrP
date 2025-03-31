05225562db3367ca05cb85526741c39857cc582f
MrP <michael.plucinski@providenceacademy.org>
2025-03-30 21:20:39 -0500

    Ask for name

--- myGitDemoMrP.cpp
+++ myGitDemoMrP.cpp
@@ -4,5 +4,9 @@ using namespace std;
 int main(){
     cout << "Greetings! " << endl;
     cout << "And Salutations!" << endl;
+    cout << "What is your name so I can know you better? " << endl;
+    string yourName;
+    cin >> yourName;
+    cout << "Greetings again, " << yourName << ", It's good to meet you!" << endl;
     return 0;
 }
